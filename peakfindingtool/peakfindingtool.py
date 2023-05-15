c1e9fb0785b323aa4111a325c30f6a1f0926daf4
Gossty <stivengolzari@gmail.com>
2023-05-15 12:59:08 -0700

    Create peakfindingtool.py

---
+++ peakfindingtool/peakfindingtool.py
@@ -0,0 +1,8 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='Peak Finding Tool',
+        description='Finds Peaks in the provided tag directory'
+    )
+    pass
